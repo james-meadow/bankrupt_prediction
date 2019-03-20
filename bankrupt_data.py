563f6f491f3ed92a614416c44daa32a71b11bfe4
james.meadow <james.meadow@slalom.com>
2019-03-20 11:56:34 -0700

    add additional columns

--- bankrupt_data.py
+++ bankrupt_data.py
@@ -1,31 +1,33 @@
 import pandas as pd
-
 import numpy as np 
 import tensorflow as tf
 
 
 """
+Dataset from here: 
+https://archive.ics.uci.edu/ml/datasets/Polish+companies+bankruptcy+data# 
+
 CSV prep: 
-cat 1year.csv | cut -d, -f 1,2,3,65 > 1year_col1.csv
-head -n 7000 1year_col1.csv > 1year_train.csv
-head -n 1 1year_col1.csv > 1year_test.csv
-tail -n 500 1year_col1.csv >> 1year_test.csv
+cat 1year.csv | cut -d, -f 1,2,3,4,5,65 > 1year_cols.csv
 """
 
-FULL_DATA = "~/Desktop/data/1year_col1.csv"
+FULL_DATA = "~/Desktop/data/1year_cols.csv"
 TRAIN_DATA = "~/Desktop/data/1year_train.csv"
 TEST_DATA = "~/Desktop/data/1year_test.csv"
 
-CSV_COLUMN_NAMES = ['Attr1','Attr2','Attr3','class']
+CSV_COLUMN_NAMES = ['Attr1','Attr2','Attr3','Attr4','Attr5','class']
 RESPONSE = [0, 1]
 
 
 def treat_data(FULL_DATA, TRAIN_DATA, TEST_DATA): 
     full = pd.read_csv(FULL_DATA, names=CSV_COLUMN_NAMES, header=0) 
-    
+    print(full)
+    print(full.dtypes)
     full = full[full.Attr1 != '?']
     full = full[full.Attr2 != '?']
     full = full[full.Attr3 != '?']
+    full = full[full.Attr4 != '?']
+    full = full[full.Attr5 != '?']
 
     is_train = np.random.uniform(0, 1, len(full)) <= .8
     train, test = full[is_train == True], full[is_train == False]
