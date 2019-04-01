5ed68f5aa95b2f1f6f9e84df4706bd0535697837
james.meadow <james.meadow@slalom.com>
2019-04-01 10:57:26 -0700

    deployed v3, using `deploy_cmle.sh`

--- deploy_cmle.sh
+++ deploy_cmle.sh
@@ -4,31 +4,35 @@
 MODEL_NAME=bankrupt_prediction 
 REGION=us-central1 
 BUCKET_NAME=bankrupt-prediction
+OUTPUT_PATH=gs://$BUCKET_NAME/model
+# MODEL_BINARIES=gs://$BUCKET_NAME/model/model/
+# MODEL_BINARIES=gs://$BUCKET_NAME/model/1554057935/
+MODEL_BINARIES=gs://$BUCKET_NAME/model/1554098440/
 
+## create model placeholder 
 gcloud ml-engine models create $MODEL_NAME --regions=$REGION
 
-
-OUTPUT_PATH=gs://$BUCKET_NAME/model
+## list the bucket 
 gsutil ls -r $OUTPUT_PATH
-## bankrupt-prediction/model/1554057935/
-MODEL_BINARIES=gs://$BUCKET_NAME/model/1554057935/
+gsutil ls -r $MODEL_BINARIES
 
-gcloud ml-engine versions create v1 \
+## create a new version of an existing model 
+gcloud ml-engine versions create v3 \
     --model $MODEL_NAME \
     --origin $MODEL_BINARIES \
     --runtime-version 1.13
 
-
+## Send a prediction using a json file. 
 gcloud ml-engine predict \
     --model $MODEL_NAME \
-    --version v1 \
+    --version v3 \
     --json-instances test_prediction.json
 
 
 ## currently stopped here. not sure what the model wants for input 
-(p3env) S02233-MBPR:bankrupt_prediction james.meadow$ gcloud ml-engine predict     --model $MODEL_NAME     --version v1     --json-instances test_prediction.json
-{
-  "error": "Prediction failed: Error during model execution: AbortionError(code=StatusCode.INVALID_ARGUMENT, details=\"Endpoint \"Placeholder:0\" fed more than once.\")"
-}
+# (p3env) S02233-MBPR:bankrupt_prediction james.meadow$ gcloud ml-engine predict     --model $MODEL_NAME     --version v1     --json-instances test_prediction.json
+# {
+#   "error": "Prediction failed: Error during model execution: AbortionError(code=StatusCode.INVALID_ARGUMENT, details=\"Endpoint \"Placeholder:0\" fed more than once.\")"
+# }
 
 
--- trainer/bankrupt_data.py
+++ trainer/bankrupt_data.py
@@ -169,41 +169,8 @@ def eval_input_fn(features, labels, batch_size):
     # Return the dataset.
     return dataset
 
-# def serving_input_fn():
-#     feature_placeholders = {
-#         'id': tf.placeholder(tf.string, [None], name="id_placeholder"),
-#         'feat': tf.placeholder(tf.float32, [None, 64], name="feat_placeholder"),
-#     }
-#     # return feature_placeholders 
-#     return input_fn_utils.InputFnOps(
-#         feature_placeholders,
-#         None,
-#         feature_placeholders)
-
-
-# feature_placeholders = {}
-# keys = CSV_COLUMN_NAMES[:-1]
-# values = tf.placeholder(tf.float32, [None])
-# for i in keys:
-#     feature_placeholders[i] = values  
-
-# keys = ['Attr1','Attr2','Attr3','Attr4','Attr5','Attr6','Attr7','Attr8','Attr9','Attr10','Attr11','Attr12','Attr13','Attr14','Attr15','Attr16','Attr17','Attr18','Attr19','Attr20','Attr21','Attr22','Attr23','Attr24','Attr25','Attr26','Attr27','Attr28','Attr29','Attr30','Attr31','Attr32','Attr33','Attr34','Attr35','Attr36','Attr37','Attr38','Attr39','Attr40','Attr41','Attr42','Attr43','Attr44','Attr45','Attr46','Attr47','Attr48','Attr49','Attr50','Attr51','Attr52','Attr53','Attr54','Attr55','Attr56','Attr57','Attr58','Attr59','Attr60','Attr61','Attr62','Attr63','Attr64']
-# values = [0.20055,0.37951,0.39641,2.0472,32.351,0.38825,0.24976,1.3305,1.1389,0.50494,0.24976,0.6598,0.1666,0.24976,497.42,0.73378,2.6349,0.24976,0.14942,43.37,1.2479,0.21402,0.11998,0.47706,0.50494,0.60411,1.4582,1.7615,5.9443,0.11788,0.14942,94.14,3.8772,0.56393,0.21402,1.741,593.27,0.50591,0.12804,0.66295,0.051402,0.12804,114.42,71.05,1.0097,1.5225,49.394,0.1853,0.11085,2.042,0.37854,0.25792,2.2437,2.248,348690,0.12196,0.39718,0.87804,0.001924,8.416,5.1372,82.658,4.4158,7.4277]
-# FEATURES = dict(zip(keys, values))
-
-# def serving_input_fn():
-#     inputs = FEATURES
-#     dataset = tf.data.Dataset.from_tensor_slices(inputs)
-#     return dataset 
-
-
-
-## still need to deploy and test this with json. 
-## now each placeholder has a distinct name
-## hopefully that solves 1 error. 
 
 def serving_input_fn():
-    # feature_placeholders = {'attr': tf.placeholder(tf.float32, [None])}
     feature_placeholders = {}
     keys = CSV_COLUMN_NAMES[:-1]
     
