abbb2de17b546c8885159f7a101571e0a94a89bd
jasonshi11 <jason.shi@slalom.com>
2019-04-02 14:25:21 -0700

    update

--- deploy_cmle.sh
+++ deploy_cmle.sh
@@ -3,7 +3,7 @@
 
 ##################################################
 ## Run this each time a new model is put into production
-##   Change MODEL_BINARIES and version number
+##   Change deploymentUri and version number
 ##   The last command will send a
 ##   single prediction to test the deployment
 ##################################################
@@ -12,27 +12,29 @@
 MODEL_NAME=bankrupt_prediction
 REGION=us-central1
 BUCKET_NAME=bankrupt-prediction
-OUTPUT_PATH=gs://$BUCKET_NAME/model
-MODEL_BINARIES=gs://$BUCKET_NAME/model/1554098440/
+#OUTPUT_PATH=gs://$BUCKET_NAME/model
+deploymentUri=gs://$BUCKET_NAME/model/1554098440/
 VERSION_NAME=v3
+INPUT_FILE=test_prediction.json
 
 ## create model placeholder
 ## only use if creating a completely new named model
 # gcloud ml-engine models create $MODEL_NAME --regions=$REGION
 
 ## list the bucket
-gsutil ls -r $OUTPUT_PATH
-gsutil ls -r $MODEL_BINARIES
+#gsutil ls -r $OUTPUT_PATH
+#gsutil ls -r $deploymentUri
 
 ## create a new version of an existing model
 ## Only use if deploying a new version
 # gcloud ml-engine versions create $VERSION_NAME \
 #     --model $MODEL_NAME \
-#     --origin $MODEL_BINARIES \
+#     --origin $deploymentUri \
 #     --runtime-version 1.13
+#     --python-version=2.7
 
 ## Send a prediction using a json file.
 gcloud ml-engine predict \
     --model $MODEL_NAME \
     --version $VERSION_NAME \
-    --json-instances test_prediction.json
+--json-instances $INPUT_FILE
